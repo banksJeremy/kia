4a8daaf41e2c5f0c6460b0512fc13297ddbf2e3d
Jeremy Banks <jeremy@jeremybanks.com>
2011-04-13 23:47:07 -0400

    m

--- main.py
+++ main.py
@@ -3,29 +3,66 @@ import functools
 import greenlet
 
 class Generator(object):
+    """A greenlet-based generator implementation.
+    
+    Use the @generator decorator on a function using the generate() function
+    to yield values. .next() and .send() behave as the built-in. Non-generator
+    functions called from generators can call generate() themselves, allowing
+    things like generate_from(), which behaves like the proposed "yield from"
+    keyword. """
+    
     def __init__(self, f, *a, **kw):
-        self.greenlet = greenlet.greenlet(functools.partial(f, *a, **kw))
+        self.f = f
+        self.a = a
+        self.kw = kw
+        
+        self.greenlet = greenlet.greenlet(self.__switched_out_call)
+        self.greenlet.switch() # will immidiately switch out
     
     def __iter__(self):
         return self
     
+    def __switched_out_call(self):
+        greenlet.getcurrent().parent.switch()
+        self.f(*self.a, **self.kw)
+    
     def next(self):
-        value = self.greenlet.switch()
+        return self.send(None)
+    
+    def send(self, value):
+        result = self.greenlet.switch(value)
         
         if self.greenlet.dead:
             raise StopIteration()
         else:
-            return value
-
-def generate(value):
-    return greenlet.getcurrent().parent.switch(value)
+            return result
 
 def generator(f):
     return functools.partial(Generator, f)
 
+def generate(value):
+    return greenlet.getcurrent().parent.switch(value)
+
+def generate_from(iterable):
+    # Generates all of the values from a generator, returning the last value
+    # passed into the generator.
+    
+    result = None
+    
+    for value in iterable:
+        result = generate(value)
+    
+    return result
+
 @generator
-def oneToTen():
-    for x in range(1, 11):
+def oneTo(n):
+    for x in range(1, n + 1):
         generate(x)
 
-print list(oneToTen())
+@generator
+def oneTwo(n, m):
+    generate_from(oneTo(n))
+    generate_from(oneTo(m))
+    generate_from([10, 20])
+
+print list(oneTwo(10, 5))
