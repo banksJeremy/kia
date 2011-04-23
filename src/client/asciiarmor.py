cbabcd588fdae1ac70fd21f8730ed060ae909bdf
Jeremy Banks <jeremy@jeremybanks.com>
2011-04-23 13:57:43 -0400

    Heavily modified crypto.py using asciiarmor.py working(?)

--- src/client/asciiarmor.py
+++ src/client/asciiarmor.py
@@ -18,9 +18,9 @@ class AsciiArmored(object):
     AsciiArmored().dumps() writes a string.
     """
     
-    def __init__(self, data=None, type=None, headers=None):
+    def __init__(self, data=None, type_=None, headers=None):
         self.data = data or binary.ByteArray()
-        self.type = type or "PGP MESSAGE"
+        self.type = type_ or "PGP MESSAGE"
         self.headers = headers or []
     
     @classmethod
@@ -38,20 +38,23 @@ class AsciiArmored(object):
                 armor_header_type = armor_header_match.group("type")
                 
                 armor_header_line_index = index
+                break
         else:
-            raise ValueError("No ASCII Armor in input!")
+            raise ValueError("No ASCII Armor in input!", s)
         
         # read ascii armors headers
         
         armor_headers = []
         
-        for index, line in enumerate(lines[armor_header_line_index + 1:]):
+        for index, line in enumerate(lines[armor_header_line_index + 1:],
+                                     start=armor_header_line_index + 1):
             if ": " in line:
                 name, _, value = line.partition(": ")
                 armor_headers.append((name, value))
                 index_after_armor_headers = index + 1
             else:
                 index_after_armor_headers = index
+                break
         else:
             raise ValueError("Unexpected end of input; "
                              "expecting ASCII Armor headers or data.")
@@ -60,7 +63,8 @@ class AsciiArmored(object):
         
         base64data = ""
         
-        for index, line in enumerate(lines[index_after_armor_headers:]):
+        for index, line in enumerate(lines[index_after_armor_headers:],
+                                     start=index_after_armor_headers):
             armor_tail_match = re.match("^-{5}END( (?P<type>.+))?-{5}$", line)
             
             canonized_line = re.sub("[^A-Za-z0-9\\+\\/\\=]", "", line)
@@ -74,15 +78,16 @@ class AsciiArmored(object):
         
         data = binary.ByteArray(base64.b64decode(base64data))
         
-        return cls(armor_header_type, armor_headers, data)
+        return cls(data=data, type_=armor_header_type, headers=armor_headers)
     
     def dumps(self):
-        parts = []
+        lines = []
         
-        parts.append("-----BEGIN " + (self.type) + "-----\n")
-        parts.append(textwrap.wrap(base64.b64encode(self.data, 65)))
-        parts.append("\n-----BEGIN " + (self.type) + "-----\n")
+        lines.append("-----BEGIN " + (self.type) + "-----")
+        lines.extend(textwrap.wrap(base64.b64encode(self.data), 64))
+        lines.append("-----END " + (self.type) + "-----")
+        lines.extend([])
         
-        return "".join(parts)
+        return "\n".join(lines)
 
 loads = AsciiArmored.loads
--- src/client/crypto.py
+++ src/client/crypto.py
@@ -1,54 +1,76 @@
 #!../../bin/python2.7
 from __future__ import division, print_function, unicode_literals
 
-import base64
 import hashlib
 import tempfile
 
 import M2Crypto
 
-# TODO:
-# Stop using ASCII IO, use M2Crypto.BIO.MemoryBuffer
+import binary
+import asciiarmor
+import base64
 
 def noop(*a, **kw):
     "Some M2Crypto functions require callbacks to shut up."
 
-class Key(object): # trolololol
+class Key(object):
     # A class wrapping the functionality needed to do RSASSA-PSS signing
     # and verification as recommended in Colin Percival's "Everything you
     # need to know about cryptography in 1 hour" (http://goo.gl/QD92C).
-    #
-    # In theory. I'm haven't used crypto stuff very much.
-    
-    def __init__(self, as_ascii=None):
-        if as_ascii is None:
+        
+    def __init__(self, type_="private", key_data=None):
+        if key_data is None:
             self._key = M2Crypto.RSA.gen_key(2048, 65537, noop)
             self.type = "private"
+        else:
+            self.type = type_
             
+            with tempfile.NamedTemporaryFile("w+t") as f:
+                if self.type == "private":
+                    data_type = "RSA PRIVATE KEY"
+                else:
+                    data_type = "PUBLIC KEY"
+                
+                self._key_data = key_data
+                
+                armored = asciiarmor.AsciiArmored(data=key_data, type_=data_type)
+                
+                f.write(armored.dumps())
+                
+                f.flush()
+                
+                if self.type == "private":
+                    self._key = M2Crypto.RSA.load_key(f.name)
+                else:
+                    self._key = M2Crypto.RSA.load_pub_key(f.name)
+    
+    @property
+    def key_data(self):
+        if self._key_data is not None:
             with tempfile.NamedTemporaryFile("w+t") as f:
                 self._key.save_key(f.name, cipher=None, callback=noop)
                 
                 f.seek(0)
-                self.as_ascii = f.read()
-        else:
-            self.as_ascii = as_ascii
-            
-            with tempfile.NamedTemporaryFile("w+t") as f:
-                f.write(as_ascii)
-                f.flush()
                 
-                if as_ascii.startswith("-----BEGIN RSA PRIVATE KEY-----"):
-                    self.type = "private"
-                    self._key = M2Crypto.RSA.load_key(f.name)
-                else:
-                    self.type = "public"
-                    self._key = M2Crypto.RSA.load_pub_key(f.name)
+                pem = f.read()
+                
+                self._key_data = asciiarmor.loads(pem).data
+        return self._key_data
+    _key_data = None
     
-    def __repr__(self):
-        return "{0}(as_ascii={1!r})".format(type(self).__name__, self.as_ascii)
-    
-    _pub = None
-    _pub_as_ascii = None
+    def __get_pub_key_data(self):
+        """Loads the public key binary.ByteArray() from the underlying key.
+        
+        Not meant to be used directly: use .pub.key_data instead."""
+        
+        with tempfile.NamedTemporaryFile("w+t") as f:
+            self._key.save_pub_key(f.name)
+            
+            f.seek(0)
+            
+            pem = f.read()
+            
+            return asciiarmor.loads(pem).data
     
     @property
     def pub(self):
@@ -56,26 +78,17 @@ class Key(object): # trolololol
             return self
         else:
             if self._pub is None:
-                self._pub = type(self)(self.pub_as_ascii)
+                self._pub = type(self)("public", self.__get_pub_key_data())
             
             return self._pub
+    _pub = None
+    
+    def digest(self, data):
+        return hashlib.sha256(data).digest()
     
     @property
-    def pub_as_ascii(self):
-        if self.type == "public":
-            return self.as_ascii
-        else:
-            if self._pub_as_ascii is None:
-                with tempfile.NamedTemporaryFile("w+t") as f:
-                    self._key.save_pub_key(f.name)
-                    
-                    f.seek(0)
-                    self._pub_as_ascii = f.read()
-            
-            return self._pub_as_ascii
-    
-    def digest(self, message):
-        return hashlib.sha256(message).digest()
+    def domain_id(self):
+        return base64.b32encode(self.digest(self.pub.key_data)).lower()
     
     def verify(self, message, signature):
         return bool(self._key.verify_rsassa_pss(self.digest(message), signature, "sha256")) 
@@ -85,60 +98,27 @@ class Key(object): # trolololol
             return self._key.sign_rsassa_pss(self.digest(message), "sha256")
         else:
             raise Exception("Private key required to sign messages.")
-    
-    _domain_id = None
-    
-    @property
-    def domain_id(self):
-        # this is a nonstandard way of generating an identifier for a
-        # key. it's used for "domain names".
-        # 
-        # it is not particularly intelligent, but I see know reason why
-        # it shouldn't work fine.
-        # 
-        # Strip the padding and spacing from the radix64ed public key, sha256
-        # it, lowercase-base32-encode the digest and strip the padding from
-        # that as well.
-        
-        if self._domain_id is None:
-            before = True
-            stripped_key = ""
-        
-            for line in self.pub.as_ascii.split("\n"):
-                stripped_line = line.strip("\n\r\t =")
-            
-                if before:
-                    if stripped_line != "-----BEGIN PUBLIC KEY-----":
-                        before = False
-                else:
-                    if stripped_line == "-----END RSA PRIVATE KEY-----":
-                        break
-                    else:
-                        stripped_key += stripped_line
-        
-            digest = self.digest(stripped_key)
-            base32ed = base64.b32encode(digest)
-            cleaned = base32ed.replace("=", "").lower()
-            
-            self._domain_id = cleaned
-        
-        return self._domain_id
 
 def main():
     import crypto
     
-    print "Generating keypair..."
+    print("Generating keypair...")
     
     private = crypto.Key()
+    
+    print("Loading public key...")
+    
     public = private.pub
     
-    print "Key id:", public.domain_id
+    print("Key's domain id:", public.domain_id)
+    
+    print("Signing message...")
     
     message = "Hello, world!"
     signature = private.sign(message)
     
-    print "Verifying signature using public key..."
-    print public.verify(message, signature)
+    print("Verifying signature using public key...")
+    print(public.verify(message, signature))
 
 if __name__ == "__main__":
     import sys
