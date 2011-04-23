5931fc3bdccf203ef8321b557d3cc632375053d2
Jeremy Banks <jeremy@jeremybanks.com>
2011-04-23 05:39:24 -0400

    m

--- src/bloomfilter.py
+++ src/bloomfilter.py
@@ -4,9 +4,46 @@ from __future__ import division
 import hashlib
 import math
 
+"""
+false positive rate of Bloom Filters:
+
+bits = size * 8
+
+P(bit set by a hash)
+                     = 1 / bits
+
+P(bit not set by a hash)
+                     = 1 - P(bit set by a hash)
+                     = 1 - 1 / bits
+
+P(bit not set by any of hash_count hashes)
+                     = P(bit not set by a hash) ** hash_count
+                     = (1 - 1 / bits) ** hash_count
+
+P(bit not set by any of hash_count * capacity hashes)
+                     = P(bit not set by any of hash_count hashes) ** capacity
+                     = (1 - 1 / bits) ** (hash_count * capacity)
+
+P(bit set by one of hash_count * capacity hashes)
+                     = 1 - P(bit not set by any of hash_count * capacity hashes)
+                     = 1 - (1 - 1 / bits) ** (hash_count * capacity)
+
+P(hash_count bits set by any of hash_count * capacity hashes)
+                     = P(bit set by one of hash_count * capacity hashes) ** capacity
+                     = (1 - (1 - 1 / bits) ** (hash_count * capacity)) ** hash_count
+
+model_positive_rate  
+                     = P(hash_count bits set by any of hash_count * capacity hashes)
+                     = (1 - (1 - 1 / bits) ** (hash_count * capacity)) ** hash_count
+
+measured_positive_rate
+                     = (bits_set / bits) ** hash_count
+
+"""
+
 class BloomFilter(object):
     @classmethod
-    def of(cls, values=None, capacity=None, false_positive_rate=None):
+    def of(cls, values=None, capacity=None, positive_rate=None):
         if values is not None:
             values = list(values)
             
@@ -16,42 +53,56 @@ class BloomFilter(object):
             if capacity is None:
                 raise ValueException("capacity or values must be specified ")
         
-        if false_positive_rate is None:
-            raise ValueException("false_positive_rate must be specified")
-        else:
-            size = 0
-            
-            self = cls(size)
-            
+        if positive_rate is None:
+            raise ValueException("positive_rate must be specified")
+        
+        # coppied blindly from Wikipedia
+        size = math.ceil(((capacity * math.log(positive_rate)) / (math.log(2) ** 2)) / 8)
+        hash_count = math.ceil(((size * 8) / capacity) * math.log(2))
+        
+        self = cls(size, hash_count)
+        
         if values is not None:
             for value in values:
                 self.add(value)
         
         return self
     
-    def __init__(self, data, salt=""):
-        self.state = binary(data)
+    def __init__(self, data_or_size, hash_count, salt=""):
+        self.state = binary(data_or_size)
+        self.hash_count = int(hash_count)
         self.salt = binary(salt)
     
+    @property
+    def positive_rate(self):
+        # the ratio of results that are going to be positive whether they
+        # should be or not.
+        
+        if self._positive_rate is None:
+            self._positive_rate = (self.state.count_bits() / (len(self.state) * 8)) ** hash_count
+        
+        return self._positive_rate
+    _positive_rate = None
+    
     def add(self, value):
+        self._false_positive_rate = None
+        
         pass
     
+    
     def contains(self, value):
+        # returns the probability that the given value is in the set
+        # (integer 0 if not found, otherwise float > 0.0; ≤ 1.0.)
+        
+        return
+        
         pass
     
     def __contains__(self, value):
-        return self.contains(self, value)
-        
+        return bool(self.contains(self, value))
 
-def hash_int(data, low, high, salt="", hash_type=hashlib.sha256):
-    # generates a integer in a given range from a hash of a value
-    # perhaps slightly overweighted to the low end
-    
-    bytes = int(math.log(high - low, 256) + 8)
-    
-    data = sized_hash(data, bytes=bytes, salt=salt, hash_type=hash_type)
-    
-    return (int(data) % (high - low)) + low
+def int_hash(data, low, high, salt="", hash_type=hashlib.sha256):
+    raise NotImplementedError()
 
 def sized_hash(data, bytes=32, salt="", hash_type=hashlib.sha256):
     hashing = hash_type(salt)
@@ -71,6 +122,29 @@ def sized_hash(data, bytes=32, salt="", hash_type=hashlib.sha256):
 
 class binary(bytearray):
     # extends bytearray to support some binary operations
+    
+    @classmethod
+    def from_int(cls, value):
+        backward_bytes = cls()
+        value = int(value)
+        
+        while value > 0:
+            backward_bytes += binary([value & 255])
+            value >>= 8
+        
+        return backward_bytes
+    
+    def get_bit(self, i):
+        raise NotImplementedError()
+    
+    def set_bit(self, i, value):
+        raise NotImplementedError()
+    
+    def __add__(self, other):
+        return type(self)(bytearray.__add__(self, other))
+    
+    def __iadd__(self, other):
+        return bytearray.__iadd__(self, other)
     
     def __or__(self, other):
         return type(self)((a | b) for (a, b) in zip(self, other))
