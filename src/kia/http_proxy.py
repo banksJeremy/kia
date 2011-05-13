7d3dbff56882e7af45b1ab60ea8dac01898c27cd
Jeremy Banks <jeremy@jeremybanks.com>
2011-05-12 21:17:28 -0400

    figured out how to work an http proxy

--- src/kia/http_proxy.py
+++ src/kia/http_proxy.py
@@ -1,12 +1,29 @@
-#!../../bin/python
-from twisted.web import proxy, http
-from twisted.internet import reactor
-from twisted.python import log
+#!/usr/bin/env python2.7
 import sys
-log.startLogging(sys.stdout)
- 
-class ProxyFactory(http.HTTPFactory):
-    protocol = proxy.Proxy
- 
-reactor.listenTCP(8080, ProxyFactory())
-reactor.run()
+
+import twisted.internet
+import twisted.web.proxy
+import twisted.web.http
+import twisted.python.log
+
+class KiaProxyRequest(twisted.web.proxy.ProxyRequest):
+    def process(self):
+        __import__("pprint").pprint(self.__dict__)
+        
+        self.received_headers["host"] += ".kia"
+        
+        twisted.web.proxy.ProxyRequest.process(self)
+
+class KiaProxy(twisted.web.proxy.Proxy):
+    requestFactory = KiaProxyRequest
+    
+    def __init__(self, *a, **kw):
+        twisted.web.proxy.Proxy.__init__(self, *a, **kw)
+        
+        self.state = "la la la"
+
+class KiaHTTPProxyFactory(twisted.web.http.HTTPFactory):
+    protocol = KiaProxy
+
+twisted.internet.reactor.listenTCP(12804, KiaHTTPProxyFactory())
+twisted.internet.reactor.run()
